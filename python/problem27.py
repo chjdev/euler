0805da364c5f9fe5e46b13898ac532f1ea781aff
chjdev <chjdev@users.noreply.github.com>
2014-10-09 15:06:38 +1100

    new solutions

--- python/problem27.py
+++ python/problem27.py
@@ -22,43 +22,70 @@
 # Find the product of the coefficients, a and b, for the quadratic expression that produces the maximum number of
 # primes for consecutive values of n, starting with n = 0.
 
+from itertools import count
 import math
 
-
-def _gen_sub_primes(cur, primes, lst_size):
-    lst = list(range(cur, cur + lst_size * 2))
-
-    lst[0] = 0
-    for prime in filter(lambda p: p > 0, primes):
-        for j in range(prime - (cur % prime), len(lst), prime):
-            lst[j] = 0
-
-    return lst
+MAX_A = MAX_B = 1000
 
 
-def _spout_primes(cur, primes):
-    for i in range(cur, len(primes)):
-        prime = primes[i]
-        if prime != 0:
-            yield primes[i]
+class Primes:
+    def __init__(self):
+        self.sieve = [False, False, True, True, False, True, False, True]  # init to 0 - 3
+
+    def _expand(self):
+        length = len(self.sieve)
+        new_length = 2 * length
+        right = [True,] * length
+        for num in filter(lambda i: self.sieve[i], range(0, len(self.sieve))):
+            i = math.ceil(length / num)
+            pos = num * i
+            while pos < new_length:
+                right[pos - length] = False
+                i += 1
+                pos = num * i
+        self.sieve = self.sieve + right
+
+    def __getitem__(self, num):
+        if num < 2:
+            return False
+
+        while num >= len(self.sieve):
+            self._expand()
+
+        return self.sieve[num]
 
 
-def gen_primes():
-    cur = 0
-    primes = [0, 0, 2, 3]
-    lst_size = len(primes)
-
-    while True:
-        yield from _spout_primes(cur, primes)
-        cur = len(primes)
-        lst_size *= 2
-        primes += _gen_sub_primes(cur, primes, lst_size)
+PRIMES = Primes()
 
 
-def create_candidates():
-    return [(a, b) for a in range(-1000, 1000) for b in range(-1000, 1000)]
+def quadratic_formula(n, factors):
+    a, b = factors
+    return n**2 + a*n + b
 
 
-candidates = create_candidates()
-n = 0
-    candidates = filter(lambda pair: pai)
+def calc_num_primes(factors):
+    num = 0
+    for n in count():
+        possible_prime = quadratic_formula(n, factors)
+        if PRIMES[possible_prime]:
+            num += 1
+        else:
+            return num
+
+candidates = ((a, b)
+              for b in range(-MAX_B + 1, MAX_B) if PRIMES[b]  # b has to be a prime, otherwise n=0 is prime
+              for a in range(-MAX_A + 1, MAX_A) if a % 2 == 1)  # a has to be odd, so n=1 is odd
+num_primes = {}
+for candidate in candidates:
+    num = calc_num_primes(candidate)
+    num_primes[candidate] = num
+
+max = 0
+max_candidate = (0, 0)
+for a, b in num_primes:
+    num = num_primes[(a, b)]
+    if num > max:
+        max = num
+        max_candidate = (a, b)
+
+print(max, max_candidate, max_candidate[0] * max_candidate[1])
